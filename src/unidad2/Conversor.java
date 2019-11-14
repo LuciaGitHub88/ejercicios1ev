450a02c15c7e16e459cd2e36589294407410c7e5
LuciaGitHub88 <56444744+LuciaGitHub88@users.noreply.github.com>
2019-11-14 17:57:52 +0100

    ejercicio 2 unidad 2 resuelto

---
+++ src/unidad2/Conversor.java
@@ -0,0 +1,21 @@
+package unidad2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Conversor {
+
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedReader teclado = new BufferedReader (new InputStreamReader(System.in));
+		float dolares;
+		float euros;
+		System.out.println("Euros ");
+		String entrada = teclado.readLine();
+		euros = Float.parseFloat(entrada);
+		dolares = euros * 1.10f;
+		System.out.printf("Dólares EEUU %.2f$",dolares);
+	}
+
+}

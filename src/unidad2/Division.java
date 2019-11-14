4ffd9785f943bb6fd9fe15aa3c8d00fd48eeaae4
LuciaGitHub88 <56444744+LuciaGitHub88@users.noreply.github.com>
2019-11-14 17:56:17 +0100

    Ejercicio 1 Unidad 2 resuelto

---
+++ src/unidad2/Division.java
@@ -0,0 +1,14 @@
+package unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		float dividendo=1234;
+		float divisor=532;
+		float resultado=1234/532;
+		System.out.printf("El resultado de la divisón de %.0f entre %.0f es de %015.2f",dividendo,divisor,resultado);
+		System.out.print("\nEl resultado de la division de "+dividendo+" entre "+divisor+" es de "+resultado);
+		
+	}
+
+}

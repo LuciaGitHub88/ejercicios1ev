1ad0ec3d44a775c76f9d7aae073987797287f4b1
LuciaGitHub88 <56444744+LuciaGitHub88@users.noreply.github.com>
2019-11-19 19:12:57 +0100

    Ejercicio 5 Unidad 2 resuelto

--- src/unidad2/Sueldo.java
+++ src/unidad2/Sueldo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Sueldo {
 
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);	
+		Scanner teclado = new Scanner(System.in);
 		System.out.print("Sueldo: ");
 		float sueldo = teclado.nextFloat();
 		System.out.print("Venta 1: ");
@@ -14,9 +14,12 @@ public class Sueldo {
 		float venta2 = teclado.nextFloat();
 		System.out.print("Venta 3: ");
 		float venta3 = teclado.nextFloat();
-		
-		System.out.printf("Total a cobrar: %.2f€\nSueldo: %.2f€\nVenta 1: %.2f€\nVenta 2: %.2f€\nVenta 3: %.2f€\nTotal comisiones: %.2f€",sueldo+(venta1*0.1)+(venta2*0.1)+(venta3*0.1),sueldo,venta1*0.1,venta2*0.1,venta3*0.1,(venta1*0.1)+(venta2*0.1)+(venta3*0.1));
-		
+
+		System.out.printf(
+				"Total a cobrar: %.2f€\nSueldo: %.2f€\nVenta 1: %.2f€\nVenta 2: %.2f€\nVenta 3: %.2f€\nTotal comisiones: %.2f€",
+				sueldo + (venta1 * 0.1) + (venta2 * 0.1) + (venta3 * 0.1), sueldo, venta1 * 0.1, venta2 * 0.1,
+				venta3 * 0.1, (venta1 * 0.1) + (venta2 * 0.1) + (venta3 * 0.1));
+		teclado.close();
 	}
 
 }
